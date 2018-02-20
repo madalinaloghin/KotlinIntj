e84a12e9354c6eb9d94dccca0c6eadb3e5a3c4d6
madalinaloghin <loghinmariamadalina@gmail.com>
2018-02-20 15:19:36 +0200

    first round

---
+++ src/com/mada/kotlin/kotlincourse/basics/Variables.kt
@@ -0,0 +1,10 @@
+package com.mada.kotlin.kotlincourse.basics
+
+fun main(args: Array<String>) {
+
+    var streetNumber = 10
+    var streetName = "High Street"
+
+
+}
+
---
+++ src/com/mada/kotlin/kotlincourse/stdlib/FilterMapFlatMap.kt
@@ -0,0 +1,2 @@
+package com.mada.kotlin.kotlincourse.stdlib
+
