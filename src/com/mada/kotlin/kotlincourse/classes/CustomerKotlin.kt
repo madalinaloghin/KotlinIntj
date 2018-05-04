c4e3792f5b9164b4e24fb40cc81c98f0ea9f90e8
madalinaloghin <loghinmariamadalina@gmail.com>
2018-05-04 15:45:22 +0300

    third

--- src/com/mada/kotlin/kotlincourse/classes/CustomerKotlin.kt
+++ src/com/mada/kotlin/kotlincourse/classes/CustomerKotlin.kt
@@ -1,10 +1,37 @@
 package com.mada.kotlin.kotlincourse.classes
 
-data class CustomerKotlin(var id: Int, var name: String, var email: String){
+import java.io.IOException
+
+data class CustomerKotlin(var id: Int, var name: String, var email: String) {
+
+    @JvmField
+    val someProperty = "value"
+
     override fun toString(): String {
 //        return "{id: $id, name: $name, email: $email}"
         return "{\"id:\":\"$id\", \"name:\":\"$name\",\"email: \":\"$email\"}"
     }
+
+    @JvmOverloads
+    fun changeStatus(status: Status = Status.Current) {
+    }
+
+    @JvmName("preferential")
+    fun makePreferred() {
+    }
+
+    @Throws(IOException::class)
+    fun loadStatistics(filename: String) {
+        if (filename == "") {
+            throw IOException("Filename cannot be blank.")
+        }
+    }
+
+}
+
+enum class Status {
+    Current,
+    Past
 }
 
 fun main(args: Array<String>) {
@@ -13,12 +40,12 @@ fun main(args: Array<String>) {
     val customer2 = CustomerKotlin(1, "Thomas", "thomas@gmail.com")
 
     val customer3 = customer1
-    val customer4  = customer1.copy(3)
+    val customer4 = customer1.copy(3)
 
     println(customer3)
     println(customer4)
 
-    if(customer1 == customer2) println("they are the same. ")
+    if (customer1 == customer2) println("they are the same. ")
 
 
 }
---
+++ src/com/mada/kotlin/kotlincourse/interop/TalkingToKotlin.java
@@ -0,0 +1,35 @@
+package com.mada.kotlin.kotlincourse.interop;
+
+import com.mada.kotlin.kotlincourse.classes.Customer;
+import com.mada.kotlin.kotlincourse.classes.CustomerKotlin;
+import com.mada.kotlin.kotlincourse.classes.Status;
+
+import java.io.IOError;
+import java.io.IOException;
+
+public class TalkingToKotlin {
+
+    public void loadStats(CustomerKotlin customerKotlin) {
+        try {
+            customerKotlin.loadStatistics("filename");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        CustomerKotlin customerKotlin = new CustomerKotlin(1, "Mada", "mada@loghin.com");
+
+        customerKotlin.getEmail();
+
+//        customerKotlin.getSomeProperty();
+        String value = customerKotlin.someProperty;
+
+        customerKotlin.changeStatus(Status.Current);
+        customerKotlin.changeStatus();
+
+        customerKotlin.preferential();
+
+    }
+}
